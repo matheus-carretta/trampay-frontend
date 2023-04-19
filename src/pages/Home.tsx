ae1c60ea511b889e9cd600b17e54c01c3262446f
matheus-carretta <matheuscarretta@gmail.com>
2023-04-18 23:46:08 -0300

    feat: change home page message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [authToken, setAuthToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('auth');
+    setAuthToken(token);
+  }, []);
+
   return (
     <>
       <Header />
-      <h1>Bem-vindo à página Home!</h1>
-      <p>Aqui você encontrará informações relevantes sobre o nosso site.</p>
+      <p>{authToken 
+          ? <span>Você pode enviar seu csv nesta <Link to="/send-balance">página</Link>.</span>
+          : <span>Faça <Link to="/login">login</Link> para enviar seu csv.</span>
+        }
+      </p>
     </>
   );
 };
